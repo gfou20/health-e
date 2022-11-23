99e039db4ef11decb176cc18d438127cd34ffe45
GFou <gfouchard20@outlook.com>
2022-11-23 16:20:09 -0500

    add home page basic outline

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Title from './components/Title/Title';
 import NavBar from './components/NavBar/NavBar';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <NavBar />
         <div className='box1'>
           <div className='box2'>
-
+            <Home />
           </div>
         </div>
     </div>
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import styles from './Home.module.css'
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Health-E</h1>
+      <img src='./images/women.png' alt='happy woman with hands up' className={styles.img} />
+      <p>Here at Health-E, our focus is giving our members the tools necessary to staying organized regarding all of their healthcare needs. Health-E offers emergency contact information, personal medical journals and scheduling appointments with any of your medical providers.</p>
+      <hr />
+      <p>To learn more about Health-E, click below!</p>
+      <button>More about Health-E</button>
+    </div>
+  )
+}
+
+export default Home
