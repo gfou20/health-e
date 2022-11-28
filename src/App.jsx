10202c0d8f8d9a86c1ecf208c5f578d5bfecb97f
GFou <gfouchard20@outlook.com>
2022-11-28 15:44:49 -0500

    add appointments basic page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Journal from './pages/Journal/Journal';
 import Contacts from './pages/Contacts/Contacts';
+import Appointments from './pages/Appointments/Appointments';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
               <Route path='/about' element={<About />} />
               <Route path='/journal' element={<Journal />} />
               <Route path='/contacts' element={<Contacts />} />
+              <Route path='/appointments' element={<Appointments />} />
             </Routes>
           </div>
         </div>
---
+++ src/pages/Appointments/Appointments.jsx
@@ -0,0 +1,12 @@
+function Appointments() {
+  return (
+    <div>
+      <h1>Appointments</h1>
+      <h3>Upcoming Appointments</h3>
+      <h3>Scheduled Appointments</h3>
+      <h3>Past Appointments</h3>
+    </div>
+  )
+}
+
+export default Appointments
