bfb02cd7e3a6a677067dfacdf23d2347a5b8ba55
GFou <gfouchard20@outlook.com>
2022-11-28 12:16:44 -0500

    edit about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,14 @@
+import styles from './About.module.css'
+
 function About() {
   return (
-    <h1>About Health-E</h1>
+    <div className={styles.about}>
+      <h1>About Health-E</h1>
+      <img src='./images/woman-two.png' alt='smiling woman' />
+      <p>Health-E is an organization focused with one goal in mind: putting the member first!</p>
+      <p>The member is able to see what new appointments have been made or any that have been missed or marked as attended. All emergency as well as physician contacts are located in one place, making it easier to keep track of important information. The member is also able to keep a medical journal, allowing them the ability to keep a record of any changes in their health.</p>
+      <p>Only during a medical emergency will a physician have access to a members journal, in case any medical issues have been mentioned unknwon to the physician. Health-E takes member data seriously and ensures all medical information remain secure.</p>
+    </div>
   )
 }
 
